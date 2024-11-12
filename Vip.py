b3366672d80c94aec8614d28e5e9d1f54a014ef6
vattoli4431 <a98759736@gmail.com>
2024-11-12 20:45:35 +0530

    Add files via upload

---
+++ Vip.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Vip_Ddos_07
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7407562931:AAExaNEj3mMr8oPvmdTrZ6KZUuEJFDowqPM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("KaliaYtOwner")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6862918172")
+
+
+
+
+
+  
